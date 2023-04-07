95335cfd44baa3ffc1127d6a03785d196ff4e75f
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-04-07 17:18:40 +0200

    Project Structure changes

---
+++ EditorRenderer/Assets/TestScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(Vector3.right * 0.1f);
+    }
+}
