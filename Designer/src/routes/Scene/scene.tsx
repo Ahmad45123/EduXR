a41bbc99eace9bbd8ec9952128a2a99cb3021e9e
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-04-15 23:02:14 +0200

    Added redirect to scenes page when scene doesn't exist.

--- Designer/src/routes/Scene/scene.tsx
+++ Designer/src/routes/Scene/scene.tsx
@@ -38,6 +38,7 @@ import { useParams } from 'react-router-dom';
 import Vector3Input from '../../components/vector3_input';
 import SceneObjectComp from './object_comp';
 import useScene from '../../core/hooks/useScene';
+import { Navigate } from 'react-router-dom';
 
 export default function Scene() {
   const { sceneName } = useParams();
@@ -57,57 +58,64 @@ export default function Scene() {
     setNewObjectName('');
   };
 
-  return (
-    <Box
-      display="flex"
-      gap="4"
-      width="100%"
-      height="100%"
-      justifyContent="space-between"
-      padding="1em"
-    >
-      <Box width="50%" height="100%">
-        <Tabs display="flex" flexDir="column" height="100%">
-          <TabList>
-            <Tab>Scene Objects</Tab>
-            <Tab>Scene Logic</Tab>
-          </TabList>
-          <TabPanels flexGrow="1">
-            <TabPanel height="100%" display="flex" flexDirection="column" gap="1em">
-              <FormControl border="1px" borderRadius="0.5em" p="1em">
-                <FormLabel>Create New Object</FormLabel>
-                <Flex gap="0.5em">
-                  <Input
-                    value={newObjectName}
-                    onChange={e => setNewObjectName(e.target.value)}
-                    placeholder="Object Name"
-                  />
-                  <Select width="20em" placeholder="Object Type" />
-                  <Button width="10em" onClick={createObject}>
-                    Create
-                  </Button>
-                </Flex>
-              </FormControl>
-              <Box flexGrow="1" flexShrink="1" flexBasis="0" overflow="auto">
-                <Flex gap="1em" wrap="wrap">
-                  {sceneCore.scene?.objects.map(obj => (
-                    <SceneObjectComp
-                      key={obj.objectName}
-                      sceneObject={sceneCore.getObject(obj.objectName)}
+  if (!sceneCore.scene) {
+    return <Navigate to="/" />;
+  } else {
+    return (
+      <Box
+        display="flex"
+        gap="4"
+        width="100%"
+        height="100%"
+        justifyContent="space-between"
+        padding="1em"
+      >
+        <Box width="50%" height="100%">
+          <Tabs display="flex" flexDir="column" height="100%">
+            <TabList>
+              <Tab>Scene Objects</Tab>
+              <Tab>Scene Logic</Tab>
+            </TabList>
+            <TabPanels flexGrow="1">
+              <TabPanel height="100%" display="flex" flexDirection="column" gap="1em">
+                <FormControl border="1px" borderRadius="0.5em" p="1em">
+                  <FormLabel>Create New Object</FormLabel>
+                  <Flex gap="0.5em">
+                    <Input
+                      value={newObjectName}
+                      onChange={e => setNewObjectName(e.target.value)}
+                      placeholder="Object Name"
                     />
-                  ))}
-                </Flex>
-              </Box>
-            </TabPanel>
-            <TabPanel height="100%">
-              <div ref={logicDesignerRef} style={{ width: '100%', height: '100%' }}></div>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
+                    <Select width="20em" placeholder="Object Type" />
+                    <Button width="10em" onClick={createObject}>
+                      Create
+                    </Button>
+                  </Flex>
+                </FormControl>
+                <Box flexGrow="1" flexShrink="1" flexBasis="0" overflow="auto">
+                  <Flex gap="1em" wrap="wrap">
+                    {sceneCore.scene?.objects.map(obj => (
+                      <SceneObjectComp
+                        key={obj.objectName}
+                        sceneObject={sceneCore.getObject(obj.objectName)}
+                      />
+                    ))}
+                  </Flex>
+                </Box>
+              </TabPanel>
+              <TabPanel height="100%">
+                <div
+                  ref={logicDesignerRef}
+                  style={{ width: '100%', height: '100%' }}
+                ></div>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+        <Box width="50%" alignSelf="center">
+          <UnityViewer style={{ width: '100%' }} />
+        </Box>
       </Box>
-      <Box width="50%" alignSelf="center">
-        <UnityViewer style={{ width: '100%' }} />
-      </Box>
-    </Box>
-  );
+    );
+  }
 }
