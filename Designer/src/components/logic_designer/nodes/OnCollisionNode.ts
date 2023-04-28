87c128c688e50128dcf6e821c759815e64b7cb6e
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-04-29 00:23:28 +0200

    Updated oncollison Node to take in the objects.

--- Designer/src/components/logic_designer/nodes/OnCollisionNode.ts
+++ Designer/src/components/logic_designer/nodes/OnCollisionNode.ts
@@ -1,12 +1,14 @@
 import { ClassicPreset } from 'rete';
 import { stringSocket, execSocket } from '../sockets';
 import { BaseNode } from '../base_types';
+import { InputBoxControl } from '../controls/InputBoxControl';
 
 export class OnCollisionNode extends BaseNode {
   constructor() {
     super('OnCollision');
 
-    this.addOutput('objName', new ClassicPreset.Output(stringSocket, 'Object Name'));
+    this.addControl('firstObject', new InputBoxControl('First Object'));
+    this.addControl('secondObject', new InputBoxControl('Second Object'));
     this.addOutput('exec', new ClassicPreset.Output(execSocket, 'Exec', false));
   }
 }
