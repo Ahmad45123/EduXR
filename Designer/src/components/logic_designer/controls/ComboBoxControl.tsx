3a34c1ea698f5659c78326268ff174390e132f01
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-05-01 01:16:26 +0200

    Fixed combobox control bug.

--- Designer/src/components/logic_designer/controls/ComboBoxControl.tsx
+++ Designer/src/components/logic_designer/controls/ComboBoxControl.tsx
@@ -1,5 +1,7 @@
 import { Select } from '@chakra-ui/react';
 import { BaseCustomControl } from './BaseCustomControl';
+import React from 'react';
+
 
 export class ComboBoxControl extends BaseCustomControl {
   constructor(public label: string, public choices: string[]) {
@@ -8,14 +10,19 @@ export class ComboBoxControl extends BaseCustomControl {
 }
 
 export function ComboBoxControlImpl(props: { data: ComboBoxControl }) {
+  const [value, setValue] = React.useState(props.data.value);
+  
   function setComboBoxValue(val: string) {
     props.data.value = val;
+      setValue(val);
   }
 
   return (
     <Select
       placeholder={props.data.label}
       width="100%"
+      value={value}
+      onPointerDown={e => e.stopPropagation()}
       onChange={e => setComboBoxValue(e.target.value as string)}
     >
       {props.data.choices.map(x => (
