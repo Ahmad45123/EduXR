c3f7da26c796ebc02326cff72d965fc00f1ff6c9
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-05-01 02:42:01 +0200

    Added new nodes to make lense experiment.

--- Designer/src/components/logic_designer/sockets/index.ts
+++ Designer/src/components/logic_designer/sockets/index.ts
@@ -1,4 +1,8 @@
 import { ClassicPreset } from 'rete';
 
+
+
 export const execSocket = new ClassicPreset.Socket('exec');
 export const stringSocket = new ClassicPreset.Socket('string');
+export const numberSocket = new ClassicPreset.Socket('number');
+export const numberOrStringSocket = new ClassicPreset.Socket('numberOrString');
