f9d9ced1b03cd3b1aa974a141a7b81905d9b0b2f
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-05-03 02:23:01 +0200

    Changed Eval library.

--- Player/Assets/Logic/Instructions/Mutators/EvalInstruction.cs
+++ Player/Assets/Logic/Instructions/Mutators/EvalInstruction.cs
@@ -4,19 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Assets.Logic.Misc;
-using org.matheval;
+using org.mariuszgromada.math.mxparser;
 
 namespace Assets.Logic.Instructions.Mutators {
     public class EvalInstruction : DataInstruction {
-        public EvalInstruction(Dictionary<string, InputParam> inputs, Dictionary<string, string> controls) : base(inputs, controls) { }
+
+        private readonly Expression _expression;
+
+        public EvalInstruction(Dictionary<string, InputParam> inputs, Dictionary<string, string> controls) : base(
+            inputs, controls) {
+            _expression = new Expression(controls["expression"]);
+            _expression.addArguments(new Argument("a", 0));
+            _expression.addArguments(new Argument("b", 0));
+        }
         public override object GetOutput(string outputName) {
             if (outputName == "output") {
-                Expression expression = new(controls["expression"]);
-                if(inputs.TryGetValue("a", out var input))
-                    expression.Bind("a", (float)input.GetValue());
-                if (inputs.TryGetValue("b", out var input1))
-                    expression.Bind("b", (float)input1.GetValue());
-                var result = expression.Eval<decimal>();
+                
+                if(inputs.TryGetValue("a", out var aInput))
+                    _expression.setArgumentValue("a", (float)aInput.GetValue());
+                if (inputs.TryGetValue("b", out var bInput))
+                    _expression.setArgumentValue("b", (float)bInput.GetValue());
+                var result = _expression.calculate();
                 return (float)result;
             }
             throw new NotImplementedException();
