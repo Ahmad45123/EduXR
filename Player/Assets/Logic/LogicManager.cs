312c1a737544f91c356762d825c921f95c2d2ac3
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2023-05-08 23:31:39 +0200

    Changed to use FixedUpdate.

--- Player/Assets/Logic/LogicManager.cs
+++ Player/Assets/Logic/LogicManager.cs
@@ -26,7 +26,7 @@ namespace Assets.Logic {
         }
 
         // Update is called once per frame
-        void Update() {
+        void FixedUpdate() {
             if (!HasStartedExecuting) return;
             foreach (var instruction in _loopInstructions) {
                 instruction.Execute();
